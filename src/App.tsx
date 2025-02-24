698f9e029e99f22225c6ca0682d53b410659dd4e
smithrowe-10 <smj0418smj@gmail.com>
2025-02-24 17:46:24 +0900

    2025-02-24 'JSX와 TSX, 컴포넌트와 CurlyBraces' 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
+import Component, { ClassComponent, FunctionalComponent } from './component/Component'
 import logo from './logo.svg';
 import './App.css';
+import CurlyBraces from './component/CurlyBraces';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      { /* <ClassComponent />
+      <FunctionalComponent />
+      <Component /> */}
+      <CurlyBraces />
     </div>
   );
 }
