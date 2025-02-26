85dafee6d52037d0a693f90ccba31deefda052bb
smithrowe-10 <smj0418smj@gmail.com>
2025-02-26 17:54:01 +0900

    2025-02-26 수업자료

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import Component, { ClassComponent, FunctionalComponent } from './component/Component'
-import logo from './logo.svg';
 import './App.css';
 import CurlyBraces from './component/CurlyBraces';
+import Properties from './component-manage/Properties';
+import Profile from './component-manage/example/Example2';
+import ConditionRender from './component-manage/ConditionRender';
+import ListRender from './component-manage/ListRender';
+import EventComponent from './interaction/EventComponent';
+import StateComponent from './interaction/StateComponent';
 
 function App() {
   return (
     <div>
-      { /* <ClassComponent />
+      {/* <ClassComponent />
       <FunctionalComponent />
       <Component /> */}
-      <CurlyBraces />
+      {/* <CurlyBraces /> */}
+      {/* <Properties /> */}
+      {/* <Profile /> */}
+      {/* <ConditionRender /> */}
+      {/* <ListRender /> */}
+      {/* <EventComponent /> */}
+      <StateComponent />
     </div>
   );
 }
